5f5cc94520cbb7a7bd4bf557d8673f666a11755f
Steven Turner <sturner@cuhsd.org>
2019-11-14 14:25:23 -0800

    Request Team 8 Names
    Include all your names in the comment on line 10.

--- team8.py
+++ team8.py
@@ -7,6 +7,7 @@
 ####
 
 team_name = 'We luv Mostafa' # Only 10 chars displayed.
+#Add all team member's names here
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
